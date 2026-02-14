2aafd68e6ae43e5c1b9f0fbf8f49d165e0ff058a
Arvind Yadav <arvind@linklumin.com>
2026-02-14 19:51:12 +0530

    learn how to call view  inside another view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 // Basic / Static Route (returns a view)
@@ -54,3 +55,4 @@ Route::get( 'contactme', function(){
 );
 
 Route::get('contact', [ContactController::class, 'contactme']);
+Route::get('product', [ProductController::class, 'show']);
