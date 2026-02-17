2d57925c334ca0f8b5a07fc7522f8ef409a5034b
126-Arvind <arvindyadav2021126@gmail.com>
2026-02-17 19:30:21 +0530

    test

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,8 @@
         <link rel="stylesheet" href="{{asset('css/all.css')}}">
         <link rel="stylesheet" href="https://fonts.google.com/share?selection.family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900">
         <title>{{$title}}</title>
-</head>
+</head> 
+
 <body>
     <div class="container-fluid bg-dark">
 <div class="row">
